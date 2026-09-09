f036e3262483a5ba0acf3455afea8cb797d5653f
CholeUnique <erhui443@gmail.com>
2026-09-09 17:27:03 +0800

    暂时推本地仓库

--- low-altitude-smart-service/components.d.ts
+++ low-altitude-smart-service/components.d.ts
@@ -16,6 +16,7 @@ declare module 'vue' {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
+    ElDialog: typeof import('element-plus/es')['ElDialog']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElInput: typeof import('element-plus/es')['ElInput']
